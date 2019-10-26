085509a6c1c6436ec013a1b5e894201fbea1e375
Sylvain Wendel <sylvain.wendel@etu.unistra.fr>
2019-10-26 14:26:06 +0200

    fin du tp

--- src/app/api-list.service.ts
+++ src/app/api-list.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, from } from 'rxjs';
 import { Product } from './model/products';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
