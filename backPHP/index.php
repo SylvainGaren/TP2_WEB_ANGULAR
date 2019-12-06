b8bea624cc8667393a9e662ff83b295fefcabb65
SylvainGaren <sylvain.wendel@hotmail.fr>
2019-12-06 09:45:39 +0000

    ajout du jeton

--- backPHP/index.php
+++ backPHP/index.php
@@ -3,15 +3,36 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
+use \Firebase\JWT\JWT;
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require 'vendor/autoload.php';
+
+
+// middleware pour le jeton
+const JWT_SECRET = "makey1234567";
+
+$jwt = new \Slim\Middleware\JwtAuthentication([
+
+"path" => "/api",
+"secure" => false,
+"secret" => JWT_SECRET,
+"passthrough" => ["/login"],
+"attribute" => "decoded_token_data",
+"algorithm" => ["HS256"],
+"error" => function ($response, $arguments) {
+                $data = array('ERREUR' => 'ERREUR', 'ERREUR' => 'AUTO');
+                return $response->withHeader("Content-Type", "application/json")->getBody()->write(json_encode($data));
+            }
+
+]);
 
 function addClient($request,$response,$args) {
 
     $body = $request->getParsedBody(); // Parse le body
-    echo $body
+    echo "Body :! " + $body;
     //$id = $body['nom']; // Data du formulaire
     $nom = $body['nom']; // Data du formulaire
     $surname = $body['prenom']; // Data du formulaire
@@ -31,7 +52,7 @@ function addClient($request,$response,$args) {
          
         // On ajoute le nouvel élement
         array_push( $tableau_pour_json, array(
-            'id' => "1",
+            'id' => "2",
             'name' => $nom,
             'surname' => $surname
         ));
@@ -40,12 +61,6 @@ function addClient($request,$response,$args) {
          
         // On stocke tout le JSON
         file_put_contents($s_file, $contenu_json);
-        echo $contenu_json;
-             
-        echo "Vos informations ont été enregistrées\n";
-        $mdr = file_get_contents($s_file);
-
-        echo $mdr;
     }
     catch( Exception $e ) {
         echo "Erreur : ".$e->getMessage();
@@ -58,6 +73,7 @@ function checkClient($request,$response,$args) {
     $body = $request->getParsedBody(); // Parse le body
     $name = $body['name']; // Data du formulaire
     $password = $body['password']; // Data du formulaire
+    echo "azdazd";
     
     try {
         // On essayes de récupérer le contenu existant
@@ -91,13 +107,32 @@ function checkClient($request,$response,$args) {
     }
 }
 
-require 'vendor/autoload.php';
+function login ($request, $response, $args) {
+    echo "login";
+    $userid = "emma";
+    $email = "emma@emma.fr";
+    $issuedAt = time();
+    $expirationTime = $issuedAt + 60; // jwt valid for 60 seconds from the issued time
+    $payload = array(
+    'userid' => $userid,
+    'iat' => $issuedAt,
+    'exp' => $expirationTime
+    );
+    $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
+    
+    $response = $response->withHeader("Authorization", "Bearer {$token}")->withHeader("Content-Type", "application/json");
+    
+    $data = array("name" => "Emma", "age" => 48);
+    return $response->withHeader("Content-Type", "application/json")->withJson($data);
+}
+
 
 $app = new \Slim\App;
 
 $app->post('/client', 'addClient');
 $app->post('/checkUser', 'checkClient');
-
+$app->get('/login', 'login');
+$app->add($jwt);
 $app->run();
 
 ?>
